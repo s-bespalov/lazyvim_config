7525a93376c42affa449d7ca823acb2ac82baa16
Sergey Bespalov <sergey.bespalov@protonmail.com>
2025-02-05 23:50:02 +0300

    configure for neovide

---
+++ lua/config/options.lua
@@ -0,0 +1,9 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+if vim.g.neovide then
+  vim.o.guifont = "Iosevka Nerd Font Mono:h12"
+  vim.g.neovide_normal_opacity = 0.9
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_fullscreen = true
+end
